8df418e26f8eb4d9bc67abe7c88d51b1cc4711af
Javier Gamarra <nhpatt@gmail.com>
2011-04-06 13:33:30 +0200

    Object Oriented Clouds (WIP2)

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@ g.addColorStop(1, '#d0e7f9');
 var loop;
 var w = c.width;
 var h = c.height;
-var numberOfClouds = 10;
+var numberOfClouds = 10, clouds = [];
 
 var clear = function() {
 	ctx.fillStyle = g;
@@ -20,31 +20,29 @@ var clear = function() {
 	ctx.fill();
 }
 
-var numberOfClouds = 10, circles = [];
 for ( var i = 0; i < numberOfClouds; i++)
-	circles.push(new Cloud(Math.random() * w,h / 2 - 2.5 * h / 2 * Math.random(),
-			Math.random() * 40, Math.random() / 2))
-var DrawCircles = function() {
+	clouds.push(new Cloud(w,h))
+var drawClouds = function() {
 	for ( var i = 0; i < numberOfClouds; i++) {
-		ctx.fillStyle = 'rgba(255, 255, 255, ' + circles[i].transparency + ')';
+		ctx.fillStyle = 'rgba(255, 255, 255, ' + clouds[i].transparency + ')';
 		ctx.beginPath();
-		ctx.arc(circles[i].positionX, circles[i].positionY, circles[i].radius, 0, Math.PI * 2,
+		ctx.arc(clouds[i].positionX, clouds[i].positionY, clouds[i].radius, 0, Math.PI * 2,
 				true);
 		ctx.closePath();
 		ctx.fill();
 	}
 };
 
-var MoveCircles = function(deltaX) {
+var moveClouds = function(deltaX) {
 	for ( var i = 0; i < numberOfClouds; i++) {
-		circles[i].move(deltaX);
+		clouds[i].move(deltaX);
 	}
 };
 
 var GameLoop = function() {
 	clear();
-	DrawCircles();
-	MoveCircles(1);
+	drawClouds();
+	moveClouds(1);
 	loop = setTimeout(GameLoop, 1000 / 50);
 }
 GameLoop();
--- js/objects.js
+++ js/objects.js
@@ -1,17 +1,24 @@
-function Cloud(positionX, positionY, radius, transparency) {
-	this.positionX = positionX;
-	this.positionY = positionY;
-	this.radius = radius;
-	this.transparency = transparency;
+const
+CLOUD_MAX_SIZE = 40;
+
+function Cloud(w, h) {
+	this.width = w;
+	this.height = h;
+	this.positionX = Math.random() * this.width;
+	this.initStartingValues();
 }
 
 Cloud.prototype.move = function(deltaX) {
-	if (this.positionX - this.radius > w) {
-		this.positionX = 0 - 2 * 40;
-		this.positionY = h / 2 - 2.5 * h / 2 * Math.random();
-		this.radius = Math.random() * 40;
-		this.transparency = Math.random() / 2;
+	if (this.positionX - this.radius > this.width) {
+		this.positionX = 0 - 2 * CLOUD_MAX_SIZE;
+		this.initStartingValues();
 	} else {
 		this.positionX += deltaX;
 	}
+}
+
+Cloud.prototype.initStartingValues = function(){
+	this.positionY = this.height / 2 - 2.5 * this.height / 2 * Math.random();
+	this.radius = Math.random() * CLOUD_MAX_SIZE;
+	this.transparency = Math.random() / 2;
 }
